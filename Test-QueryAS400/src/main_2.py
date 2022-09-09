1380f53ed9b9c7c8603e7ee42a74fef46c4d7285
cchsing <jcch950607@gmail.com>
2022-09-09 16:43:24 +0800

    update

--- Test-QueryAS400/src/main_2.py
+++ Test-QueryAS400/src/main_2.py
@@ -14,7 +14,8 @@ def main():
     daily_counter = "M" + "{:09.0f}".format(deltaD.days)
     print(daily_counter)
 
-    dir_path = pathlib.Path(__file__).parents[1]
+    # dir_path = pathlib.Path(__file__).parents[1]
+    dir_path = r"C:\Program Files\Java\jdk-18.0.2.1"
     # print(dir_path)
     jt400libpath = str(dir_path) + "\lib\jt400.jar"
     # print(jt400libpath)
@@ -26,17 +27,18 @@ def main():
     system = "PUB400.COM"
     password = "P@ssw0rd"
     jdbc_conn_url = "jdbc:as400://" + system + "/" + schema
-    aliasName = "ALIAS3"
+    db_aliasName = "ALIAS3"
 
     with jaydebeapi.connect(cls_name,
                             jdbc_conn_url,
                             [user, password],
                             jt400libpath) as conn:
         with conn.cursor() as curs:
-            curs.execute("INSERT INTO %s.%s VALUES ('3','Joke','JULIA')"%(db_library,db_table))
-            curs.execute("DROP ALIAS %s.%s"%(db_library, aliasName))
-            curs.execute(
-                "CREATE ALIAS JUSTINC951.ALIAS3 FOR JUSTINC951.TESTING001")
+            # curs.execute("INSERT INTO %s.%s VALUES ('33','Catalina','JULIA')" % (
+            #     db_library, db_table))
+            curs.execute("DROP ALIAS %s.%s" % (db_library, db_aliasName))
+            curs.execute("CREATE ALIAS %s.%s FOR %s.%s" %
+                         (db_library, db_aliasName, db_library, db_table))
             #curs.execute("SELECT COUNT(FNAME), LNAME FROM JUSTINC951.TESTING001 GROUP BY LNAME")
             curs.execute("SELECT * FROM JUSTINC951.ALIAS3")
             print(curs.fetchall())
