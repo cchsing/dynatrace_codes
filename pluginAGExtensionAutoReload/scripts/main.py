c079748d7ccbc8dc9810dac00397761c66197895
cchsing <jcch950607@gmail.com>
2022-09-19 19:00:59 +0800

    update

--- pluginAGExtensionAutoReload/scripts/main.py
+++ pluginAGExtensionAutoReload/scripts/main.py
@@ -1,7 +1,22 @@
 import requests
+import json
+import getAllExtensions
+
 
 def main():
-    print("test")
+
+    # Global Configuration
+    baseURL = "https://10.111.32.145/e/environment_id"
+
+    # Get a list of all extensions in production
+    allExtensions = getAllExtensions(baseURL=baseURL)
+
+    # Loop through the list and get the statuses of each extension
+    # if status is not ok
+    # get a list of all the instances of the extension
+    # loop through the list of instances and identify the faulty instance
+    # restart the faulty instances for N time with certain interval or increasing interval time or one time (preferable)
+
 
 if __name__ == "__main__":
     main()
