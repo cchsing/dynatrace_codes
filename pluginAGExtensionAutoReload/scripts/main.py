b77e48a392c85ea3119e43248de487cfc821b49e
cchsing <jcch950607@gmail.com>
2022-09-20 19:24:21 +0800

    updates

--- pluginAGExtensionAutoReload/scripts/main.py
+++ pluginAGExtensionAutoReload/scripts/main.py
@@ -7,13 +7,42 @@ from myModules import extensions, extInstances, config
 def main():
 
     # Global Configuration
-    baseURL = "https://10.111.32.145/e/environment_id"
+    # baseURL = "https://10.111.32.145/e/environment_id" # CIMB URL
+    baseURL = "https://isl56514.live.dynatrace.com"
     authToken = config.tokens[0]['Token']
 
     # Get a list of all extensions in production
-    allExtensions = extensions.getAllExtensions(baseURL=baseURL)
+    allExtensions = extensions.getAllExtensions(
+        baseURL=baseURL, authToken=authToken)
+    # print(json.dumps(allExtensions, indent=4))
+    as400extension = extensions.getExtension(
+        baseURL=baseURL, extId="custom.remote.python.as400_activegate_plugin", authToken=authToken)
+    # print(json.dumps(as400extension, indent=4))
+    as400extensionStates = extensions.getExtStates(
+        baseURL=baseURL, extId="custom.remote.python.as400_activegate_plugin", authToken=authToken)
+    print(json.dumps(as400extensionStates, indent=4))
+    as400extAllInstances = extInstances.getAllInstances(
+        baseURL=baseURL, extId="custom.remote.python.as400_activegate_plugin", authToken=authToken)
+    print(json.dumps(as400extAllInstances, indent=4))
+    as400extInstance = extInstances.getInstance(
+        baseURL=baseURL, extId="custom.remote.python.as400_activegate_plugin", configId="8045620730537405171", authToken=authToken)
+    print(json.dumps(as400extInstance, indent=4))
+    print('testistsetsetsefsadfa')
+    newCfgas400 = as400extInstance
+    newCfgas400['enabled'] = False
+    newCfgas400['useGlobal'] = False
+    newCfgas400['properties']['db_password'] = "P@ssw0rd"
+    print(json.dumps(newCfgas400))
+    as400extinstUpdated = extInstances.putInstance(
+        baseURL=baseURL, extId="custom.remote.python.as400_activegate_plugin", configId="8045620730537405171", authToken=authToken, body=newCfgas400)
+    print(as400extinstUpdated)
 
     # Loop through the list and get the statuses of each extension
+    # for extension in allExtensions['extensions']:
+    #     print(extension['id'])
+    #     extStats = extensions.getExtStates(
+    #         baseURL=baseURL, extId=extension['id'], authToken=authToken)
+    #     print(extStats)
     # if status is not ok
     # get a list of all the instances of the extension
     # loop through the list of instances and identify the faulty instance
--- pluginAGExtensionAutoReload/scripts/myModules/extInstances.py
+++ pluginAGExtensionAutoReload/scripts/myModules/extInstances.py
@@ -7,8 +7,10 @@ def getAllInstances(**kwargs):
     extId = kwargs['extId']
     resourcePath = "/api/config/v1/extensions/" + extId + "/instances"
     API_ENDPOINT = kwargs['baseURL'] + resourcePath
+    REQHEAD = {'accept': "application/json; charset=utf-8",
+               'Authorization': "Api-Token %s" % kwargs['authToken']}
 
-    DATA = requests.get(url=API_ENDPOINT)
+    DATA = requests.get(url=API_ENDPOINT, headers=REQHEAD)
     DATA_DICT = json.loads(DATA.text)
     return DATA_DICT
 
@@ -18,7 +20,23 @@ def getInstance(**kwargs):
     configId = kwargs['configId']
     resourcePath = "/api/config/v1/extensions/" + extId + "/instances/" + configId
     API_ENDPOINT = kwargs['baseURL'] + resourcePath
+    REQHEAD = {'accept': "application/json; charset=utf-8",
+               'Authorization': "Api-Token %s" % kwargs['authToken']}
 
-    DATA = requests.get(url=API_ENDPOINT)
+    DATA = requests.get(url=API_ENDPOINT, headers=REQHEAD)
     DATA_DICT = json.loads(DATA.text)
     return DATA_DICT
+
+
+def putInstance(**kwargs):
+    extId = kwargs['extId']
+    configId = kwargs['configId']
+    resourcePath = "/api/config/v1/extensions/" + extId + "/instances/" + configId
+    API_ENDPOINT = kwargs['baseURL'] + resourcePath
+    REQHEAD = {'Content-Type': "application/json",
+               'Authorization': "Api-Token %s" % kwargs['authToken']}
+    BODY = kwargs['body']
+
+    DATA = requests.put(API_ENDPOINT, headers=REQHEAD, data=json.dumps(BODY))
+    # DATA_DICT = json.loads(DATA.text)
+    return DATA  # DATA_DICT
