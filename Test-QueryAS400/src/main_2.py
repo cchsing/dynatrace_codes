fad57ce21b41c50ad23eca8eb906fcf55b9e75c6
Justin C <justin.chong@outlook.my>
2022-09-22 17:44:08 +0800

    updates :

--- Test-QueryAS400/src/main_2.py
+++ Test-QueryAS400/src/main_2.py
@@ -34,11 +34,11 @@ def main():
                             [user, password],
                             jt400libpath) as conn:
         with conn.cursor() as curs:
-            curs.execute("INSERT INTO %s.%s VALUES ('43','Test','Counter')" % (
-                db_library, db_table))
-            curs.execute("DROP ALIAS %s.%s" % (db_library, db_aliasName))
-            curs.execute("CREATE ALIAS %s.%s FOR %s.%s" %
-                         (db_library, db_aliasName, db_library, db_table))
+            # curs.execute("INSERT INTO %s.%s VALUES ('43','Test','Counter')" % (
+            #     db_library, db_table))
+            # curs.execute("DROP ALIAS %s.%s" % (db_library, db_aliasName))
+            # curs.execute("CREATE ALIAS %s.%s FOR %s.%s" %
+            #              (db_library, db_aliasName, db_library, db_table))
             #curs.execute("SELECT COUNT(FNAME), LNAME FROM JUSTINC951.TESTING001 GROUP BY LNAME")
             curs.execute("SELECT COUNT(*) AS COUNT FROM JUSTINC951.ALIAS3")
             # print(curs.fetchone())
